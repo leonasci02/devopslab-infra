461ba443fbfcdddc6653a7bfa403e27ab9b4e51b
Leonardo Nascimento <leonardo.nascimentoo02@hotmail.com>
2021-11-06 12:14:46 -0300

    cloud id

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,5 +1,5 @@
 # Cria uma VM no Google Cloud
 resource "google_app_engine_application" "app" {
-  project     = "devopslab-infra"
+  project     = "lab-devops-cloud-330422"
   location_id = "us-central"
 }
